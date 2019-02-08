2d705510aa5697c32b962ef8c722244713f82dce
kagematya <kagematya@gmail.com>
2019-02-09 06:21:45 +0900

    json読み込み用のクラスを追加

---
+++ Serializer/src/JSONInputArchive.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "rapidjson/document.h"
+#include "rapidjson/istreamwrapper.h"
+
+/**
+ * JSON形式を読み取る
+ */
+class JSONInputArchive {
+public:
+	JSONInputArchive(std::istream& stream)
+		: m_readStream(stream)
+		, m_document()
+	{
+		m_document.ParseStream(m_readStream);
+		assert(m_document.IsArray());
+	}
+
+	~JSONInputArchive()
+	{
+	}
+
+
+private:
+	rapidjson::IStreamWrapper m_readStream;
+	rapidjson::Document m_document;
+};
+
