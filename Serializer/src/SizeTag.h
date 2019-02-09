0e749b2c97b637ab30a2fa18585f8250a81055b7
kagematya <kagematya@gmail.com>
2019-02-10 03:31:33 +0900

    SizeTagのファイル追加忘れてた

---
+++ Serializer/src/SizeTag.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+
+/**
+ * 配列のサイズを取得するための構造
+ */
+struct SizeTag {
+	SizeTag(size_t& size) : m_size(size){}
+	size_t& m_size;
+};
+
+SizeTag make_size_tag(size_t& size) {
+	return SizeTag(size);
+}
