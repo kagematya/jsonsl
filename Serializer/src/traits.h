6a0315543fa53173966e7379a168b9fa455f66a8
kagematya <kagematya@gmail.com>
2019-02-10 14:47:52 +0900

    関数があるかどうかの判定をマクロで定義できるようにした

--- Serializer/src/traits.h
+++ Serializer/src/traits.h
@@ -3,17 +3,57 @@
 #include <type_traits>
 
 
+// T型がfuncname(Archive&)というメンバ関数を持っているかどうかを判別
+#define DEF_HAS_MEMFUN(funcname)		\
+template<typename T, typename Archive>	\
+class has_memfun_##funcname {			\
+private:								\
+	template <typename U, typename A>	\
+	static auto check(U& v, A& a) -> decltype(v.##funcname(a), std::true_type());	\
+																					\
+	static std::false_type check(...);	/* 定義がないときはこちらが有効になる */	\
+																					\
+public:																				\
+	static constexpr bool value = decltype(check(std::declval<T&>(), std::declval<Archive&>()))::value;	\
+};																					\
+/**/
+
+
+// T型に対してfuncname(Arcive&, T&)という関数が用意されているかどうかを判定
+#define DEF_HAS_FUN(funcname)			\
+template<typename T, typename Archive>	\
+class has_fun_##funcname {				\
+private:								\
+	template <typename U, typename A>	\
+	static auto check(U& v, A& a) -> decltype(##funcname(a, v), std::true_type());	\
+																					\
+	static std::false_type check(...);	/* 定義がないときはこちらが有効になる */	\
+																					\
+public:																				\
+	static constexpr bool value = decltype(check(std::declval<T&>(), std::declval<Archive&>()))::value;	\
+};																					\
+/**/
+
+// xxxxの定義があるか？
+DEF_HAS_MEMFUN(serialize);		//v.serialize(a);
+DEF_HAS_FUN(serialize);			//serialize(a, v);
+DEF_HAS_FUN(serialize_array);	//serialize_array(a, v);
+
+
+
+
+#if 0
 // T型がserialize()というメンバ関数を持っているかどうかを判別
 template<typename T, typename Archive>
 class has_memfun_serialize {
 private:
-	template<typename U, typename A>
-	static auto check(U v, A& a) -> decltype(v.serialize(a), std::true_type());
+	template <typename U, typename A>
+	static auto check(U& v, A& a) -> decltype(v.serialize(a), std::true_type());
 
 	static std::false_type check(...);		// 定義がないときはこちらが有効になる
 
 public:
-	static constexpr bool value = decltype(check(std::declval<T>(), std::declval<Archive&>()))::value;
+	static constexpr bool value = decltype(check(std::declval<T&>(), std::declval<Archive&>()))::value;
 };
 
 
@@ -42,3 +82,4 @@ private:
 public:
 	static constexpr bool value = decltype(check(std::declval<T&>(), std::declval<Archive&>()))::value;
 };
+#endif
